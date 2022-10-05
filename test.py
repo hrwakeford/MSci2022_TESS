f90072dbe8fb6be7ad2402ff03dc6c1cd989cc49
Jonas Yip <40857304+jonasyip@users.noreply.github.com>
2022-10-05 11:17:26 +0100

    test

---
+++ test.py
@@ -0,0 +1,15 @@
+import lightkurve as lk
+import matplotlib.pyplot as plt
+import numpy as np
+
+from matplotlib.transforms import (
+    Bbox, TransformedBbox, blended_transform_factory)
+from mpl_toolkits.axes_grid1.inset_locator import (
+    BboxPatch, BboxConnector, BboxConnectorPatch)
+
+from matplotlib.gridspec import GridSpec
+#%matplotlib notebook
+#%matplotlib agg
+
+print("kjd")
+
