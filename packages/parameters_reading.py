4cd7c65c08b38a10f72824970e7c8fbce6f6beb7
Jonas Yip <40857304+jonasyip@users.noreply.github.com>
2022-12-30 15:31:20 +0000

    added package: parameters_reading, added WASP_19b prob den

---
+++ packages/parameters_reading.py
@@ -0,0 +1,79 @@
+import numpy as np
+import re
+
+def read_df_params(df_param_list, df_ref_list, add_none=False):
+    """
+    Reading a row of literature values for a given parameter and converting 
+    it into an array.
+
+    Format that it will be converted to: [value, +error, -error]
+
+    Format reading examples:
+    "0.028+/-0.015" -> [0.028, 0.015, -0.015]
+    "0.3+0.5-0.1" -> [0.3, 0.5, -0.1]
+    "3.0" -> [3.0, 0.0, 0.0]
+
+
+    Parameters
+    ----------
+    df_params_list : array_like, str
+        Array of literature values of a given parameter.
+
+    df_ref_list : array_like, str
+        Array of authors/references that published the given parameter.
+
+    add_none : bool
+        False -> Only return values that authors have published
+            Ignores "---"
+        True  -> Return all values
+            Includes "---"
+
+    Returns
+    -------
+        param_list : ndarray
+            An array of [value, +error, -error].
+        
+        ref_list : ndarray, str
+            An array of authors corresponding to the parma_list.
+    
+    """
+
+    param_list = []
+    ref_list = []
+
+    p_mask = np.where(df_param_list=="---", False, True) #Masking values
+
+    for param, pmask, ref in zip(df_param_list, p_mask, df_ref_list):
+        if pmask == True:
+            if "+/-" in param: #If param in format "123+/-12"
+                param = re.split('\+\/\-', param)
+                param = [float(param[0]), float(param[1]), float('-' + param[1])]
+
+            elif ("+" in param) and ("-" in param): #If param in format "123+12-23"
+                if param[0] == "-": #If param value is negative
+                    param = e[1:]
+                    param_tmp = re.split('\+|\-', param)
+                    param = [float(param_tmp[0]), float(param_tmp[1]), float('-' + param_tmp[1])]
+
+                else: #param value is positive
+                    param_tmp = re.split('\+|\-', param)
+                    param = [float(param_tmp[0]), float(param_tmp[1]), float('-' + param_tmp[1])]
+                    
+            else: #If param value has no error
+                param = [float(param), 0.0, 0.0]
+
+            param_list.append(param)
+            ref_list.append(ref)
+
+        if (add_none == True) and (pmask == False):
+            param_list.append(None)
+            ref_list.append(ref)
+
+
+    param_list = np.array(param_list)
+    ref_list = np.array(ref_list)
+
+    return [param_list, ref_list]
+    
+
+        
